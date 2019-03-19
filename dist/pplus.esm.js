f2e67aee4150bbeb2380313233e99a7c3807a250
Jay Freestone <mail@jayfreestone.com>
2019-03-19 11:28:02 +0000

    feat(ts): Rewrite element index using enum.

--- dist/pplus.esm.js
+++ dist/pplus.esm.js
@@ -1,118 +1,98 @@
+var El;
+(function (El) {
+    El["Wrapper"] = "wrapper";
+    El["PrimaryNavWrapper"] = "primary-nav-wrapper";
+    El["PrimaryNav"] = "primary-nav";
+    El["OverflowNav"] = "overflow-nav";
+    El["ToggleBtn"] = "toggle-btn";
+    El["NavItem"] = "nav-item";
+    El["NavItems"] = "nav-items";
+})(El || (El = {}));
 function pplus(targetElem, options) {
-  const el = {
-    wrapper: undefined,
-    clonedWrapper: undefined,
-    primaryNav: undefined,
-    overflowNav: undefined,
-    toggleBtn: undefined,
-    clonedToggleBtn: undefined,
-    clonedNavItems: undefined,
-  };
-
-  const navItemMaps = {
-    primary: undefined,
-  };
-
-  const classNames = {
-    wrapper: ['p-plus'],
-    primaryNavWrapper: ['p-plus__primary-wrapper'],
-    primaryNav: ['p-plus__primary'],
-    overflowNav: ['p-plus__overflow'],
-    toggleBtn: ['p-plus__toggle-btn'],
-  };
-
-  function createMarkup() {
-    return `
-      <div data-wrapper class="${classNames.wrapper.join(' ')}">
-        <div class="${classNames.primaryNavWrapper.join(' ')}">
-          <${targetElem.tagName} 
-            data-primary-nav
-            class="${classNames.primaryNav.join(' ')}"
-          >
-            ${[].map.call(targetElem.children, elem => (
-              `<li data-nav-item>${elem.innerHTML}</li>`
-            )).join('')}
-          </${targetElem.tagName}>
-        </div>
-        <${targetElem.tagName} 
-          data-overflow-nav
-          class="${classNames.overflowNav.join(' ')}"
-        >
-        </${targetElem.tagName}>
-        <button
-          data-toggle-btn
-          class="${classNames.toggleBtn.join(' ')}"
-        >More</button>
-      <div>
-    `;
-  }
-
-  function setupEl() {
-    const markup = createMarkup();
-    const container = document.createDocumentFragment();
-
-    const original = document.createRange().createContextualFragment(markup);
-    const cloned = original.cloneNode(true);
-
-    el.wrapper = original.querySelector('[data-wrapper]');
-    el.primaryNav = original.querySelector('[data-primary-nav]');
-    el.overflowNav = original.querySelector('[data-overflow-nav]');
-    el.toggleBtn = original.querySelector('[data-toggle-btn]');
-    el.toggleBtn.style.display = 'none';
-    el.clonedWrapper = cloned.querySelector('[data-wrapper]');
-    el.primaryNavItems = original.querySelectorAll('[data-nav-item]');
-    el.clonedNavItems = cloned.querySelectorAll('[data-nav-item]');
-    el.clonedToggleBtn = cloned.querySelector('[data-toggle-btn]');
-
-    const mergedByIndex = Array.from(el.clonedNavItems)
-      .reduce((acc, item, i) => (
-        acc.concat([[item, el.primaryNavItems[i]]])
-      ), []);
-
-    navItemMaps.byClone = new Map(mergedByIndex);
-
-    container.appendChild(original);
-    container.appendChild(cloned);
-
-    targetElem.parentNode.replaceChild(container, targetElem);
-  }
-
-  function onIntersect({ target, intersectionRatio }) {
-    const targetElem = navItemMaps.byClone.get(target);
-    const navToPopulate = intersectionRatio < 1 ? 'overflowNav' : 'primaryNav';
-
-    if (!targetElem) return;
-
-    targetElem.remove();
-    el[navToPopulate].appendChild(targetElem);
-
-    updateBtnDisplay();
-  }
-
-  function updateBtnDisplay() {
-    [el.toggleBtn, el.clonedToggleBtn].forEach((btn) => {
-      btn.style.display = el.overflowNav.children.length > 0 ? 'block' : 'none';
-    });
-  }
-
-  function bindListeners() {
-    const observer = new IntersectionObserver(e => e.forEach(onIntersect), {
-      root: el.clonedWrapper,
-      rootMargin: '0px 0px 0px 0px',
-      threshold: [0, 1],
-    });
-
-    el.clonedNavItems.forEach(elem => observer.observe(elem));
-  }
-
-  (function init() {
-    setupEl();
-    bindListeners();
-  }());
-
-  return {
-    el,
-  };
+    var _a, _b, _c;
+    var el = {
+        primary: (_a = {},
+            _a[El.Wrapper] = undefined,
+            _a[El.PrimaryNav] = undefined,
+            _a[El.OverflowNav] = undefined,
+            _a[El.ToggleBtn] = undefined,
+            _a),
+        clone: (_b = {},
+            _b[El.Wrapper] = undefined,
+            _b[El.ToggleBtn] = undefined,
+            _b[El.NavItems] = undefined,
+            _b)
+    };
+    var navItemMaps = {
+        byClone: undefined
+    };
+    var classNames = (_c = {},
+        _c[El.Wrapper] = ['p-plus'],
+        _c[El.PrimaryNavWrapper] = ['p-plus__primary-wrapper'],
+        _c[El.PrimaryNav] = ['p-plus__primary'],
+        _c[El.OverflowNav] = ['p-plus__overflow'],
+        _c[El.ToggleBtn] = ['p-plus__toggle-btn'],
+        _c);
+    function cn(key) {
+        return classNames[key].join(' ');
+    }
+    function dv(key) {
+        return "data-" + key;
+    }
+    function createMarkup() {
+        return "\n      <div " + dv(El.Wrapper) + " class=\"" + cn(El.Wrapper) + "\">\n        <div class=\"" + cn(El.PrimaryNavWrapper) + "\">\n          <" + targetElem.tagName + " \n            " + dv(El.PrimaryNav) + "\n            class=\"" + cn(El.PrimaryNav) + "\"\n          >\n            " + Array.from(targetElem.children).map(function (elem) { return ("<li " + dv(El.NavItem) + ">" + elem.innerHTML + "</li>"); }).join('') + "\n          </" + targetElem.tagName + ">\n        </div>\n        <" + targetElem.tagName + " \n          " + dv(El.OverflowNav) + "\n          class=\"" + cn(El.OverflowNav) + "\"\n        >\n        </" + targetElem.tagName + ">\n        <button\n          " + dv(El.ToggleBtn) + "\n          class=\"" + cn(El.ToggleBtn) + "\"\n        >More</button>\n      <div>\n    ";
+    }
+    function setupEl() {
+        var markup = createMarkup();
+        var container = document.createDocumentFragment();
+        var original = document.createRange().createContextualFragment(markup);
+        var cloned = original.cloneNode(true);
+        el.primary[El.Wrapper] = original.querySelector("[" + dv(El.Wrapper) + "]");
+        el.primary[El.PrimaryNav] = original.querySelector("[" + dv(El.PrimaryNav) + "]");
+        el.primary[El.NavItems] = original.querySelectorAll("[" + dv(El.NavItem) + "]");
+        el.primary[El.OverflowNav] = original.querySelector("[" + dv(El.OverflowNav) + "]");
+        el.primary[El.ToggleBtn] = original.querySelector("[" + dv(El.ToggleBtn) + "]");
+        el.primary[El.ToggleBtn].style.display = 'none';
+        el.clone[El.Wrapper] = cloned.querySelector("[" + dv(El.Wrapper) + "]");
+        el.clone[El.NavItems] = cloned.querySelectorAll("[" + dv(El.NavItem) + "]");
+        el.clone[El.ToggleBtn] = cloned.querySelector("[" + dv(El.ToggleBtn) + "]");
+        var mergedByIndex = Array.from(el.clone[El.NavItems])
+            .reduce(function (acc, item, i) { return (acc.concat([[item, el.primary[El.NavItems][i]]])); }, []);
+        navItemMaps.byClone = new Map(mergedByIndex);
+        container.appendChild(original);
+        container.appendChild(cloned);
+        targetElem.parentNode.replaceChild(container, targetElem);
+    }
+    function onIntersect(_a) {
+        var target = _a.target, intersectionRatio = _a.intersectionRatio;
+        var targetElem = navItemMaps.byClone.get(target);
+        var navToPopulate = intersectionRatio < 1 ? El.OverflowNav : El.PrimaryNav;
+        if (!targetElem)
+            return;
+        targetElem.remove();
+        el.primary[navToPopulate].appendChild(targetElem);
+        updateBtnDisplay();
+    }
+    function updateBtnDisplay() {
+        [el.primary[El.ToggleBtn], el.clone[El.ToggleBtn]].forEach(function (btn) {
+            btn.style.display = el.primary[El.OverflowNav].children.length > 0 ? 'block' : 'none';
+        });
+    }
+    function bindListeners() {
+        var observer = new IntersectionObserver(function (e) { return e.forEach(onIntersect); }, {
+            root: el.clone[El.Wrapper],
+            rootMargin: '0px 0px 0px 0px',
+            threshold: [0, 1]
+        });
+        el.clone[El.NavItems].forEach(function (elem) { return observer.observe(elem); });
+    }
+    (function init() {
+        setupEl();
+        bindListeners();
+    }());
+    return {
+        el: el
+    };
 }
 
 export default pplus;
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 enum El {
   Wrapper = 'wrapper',
-    PrimaryNavWrapper = 'primary-nav-wrapper',
-    PrimaryNav = 'primary-nav',
-    OverflowNav = 'overflow-nav',
-    ToggleBtn = 'toggle-btn',
-    NavItem = 'nav-item',
-    NavItems = 'nav-items',
+  PrimaryNavWrapper = 'primary-nav-wrapper',
+  PrimaryNav = 'primary-nav',
+  OverflowNav = 'overflow-nav',
+  ToggleBtn = 'toggle-btn',
+  NavItem = 'nav-item',
+  NavItems = 'nav-items',
 }
 
 function pplus(targetElem, options) {
@@ -77,7 +77,7 @@ function pplus(targetElem, options) {
     const cloned = <Element>original.cloneNode(true);
 
     el.primary[El.Wrapper] = original.querySelector(`[${dv(El.Wrapper)}]`);
-    el.primary[El.PrimaryNav] = original.querySelector(`[${dv(El.Wrapper)}]`);
+    el.primary[El.PrimaryNav] = original.querySelector(`[${dv(El.PrimaryNav)}]`);
     el.primary[El.NavItems] = original.querySelectorAll(`[${dv(El.NavItem)}]`);
     el.primary[El.OverflowNav] = original.querySelector(`[${dv(El.OverflowNav)}]`);
     el.primary[El.ToggleBtn] = original.querySelector(`[${dv(El.ToggleBtn)}]`);
@@ -139,53 +139,3 @@ function pplus(targetElem, options) {
 }
 
 export default pplus;
-
-
-
-
-export default function pplus() {
-  const target = document.querySelector('.p-plus')
-  const primaryNav = target.querySelector('.p-plus__primary');
-  const overflowNav = target.querySelector('.p-plus__overflow');
-
-  const clonedTarget = target.cloneNode(true);
-  const clonedNavItems = Array.from(clonedTarget.querySelectorAll('.p-plus__primary > li'));
-  document.body.appendChild(clonedTarget);
-
-  function callback(e) {
-    e.forEach((item) => {
-      console.log(item, item.target);
-
-      if (item.intersectionRatio < 1) {
-        const id = item.target.getAttribute('data-item');
-        const target = primaryNav.querySelector(`li[data-item="${id}"]`);
-
-        if (!target) return;
-
-        const clone = target.cloneNode(true);
-        target.remove();
-        overflowNav.appendChild(clone);
-      } else if (overflowNav.childNodes.length) {
-        const clone = overflowNav.lastElementChild.cloneNode(true);
-        overflowNav.lastElementChild.remove();
-        primaryNav.appendChild(clone);
-      }
-
-      // else if (overflowNav.childNodes.length) {
-      //   const clone = overflowNav.lastElementChild.cloneNode(true);
-      //   overflowNav.lastElementChild.remove();
-      //   primaryNav.appendChild(clone);
-      // }
-    });
-  }
-
-  const observer = new IntersectionObserver(callback, {
-    root: clonedTarget,
-    rootMargin: '0px 0px 0px 0px',
-    threshold: [.25, .50, .75, 1],
-  });
-
-  clonedNavItems.forEach(elem => {
-    observer.observe(elem);
-  });
-};
