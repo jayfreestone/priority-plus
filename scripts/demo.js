c8a8e8c5c7f78a23e8616215b5a36ce3ebebdbad
Jay Freestone <jay.freestone@red-badger.com>
2019-05-31 13:24:33 +0100

    Further styling changes.

--- scripts/demo.js
+++ scripts/demo.js
@@ -1,5 +1,33 @@
-  const inst = pplus(document.querySelector('.js-p-target'));
+Array.from(document.querySelectorAll('.js-p-target')).forEach((target) => {
+  pplus(target);
+});
 
-  inst.on('itemsChanged', ({ detail: { overflowCount } }) => {
-    console.log('We now have this many items in the overflow: ', overflowCount);
-  });
+function resizeable(elem) {
+  const $control = elem.querySelector('.js-resizeable__control');
+  const $target = elem.querySelector('.js-resizeable__target');
+
+  function updateTargetWidth(width = $control.value) {
+    $target.style.maxWidth = `${width}%`;
+  }
+
+  function handleOnControlChange() {
+    updateTargetWidth();
+  }
+
+  $control.addEventListener('input', handleOnControlChange);
+}
+
+Array.from(document.querySelectorAll('.js-resizeable')).forEach((elem) => {
+  resizeable(elem);
+});
+
+
+// inst.on('itemsChanged', ({ detail: { overflowCount } }) => {
+//   console.log('We now have this many items in the overflow: ', overflowCount);
+// });
+
+// const inst = pplus(document.querySelector('.js-p-target'));
+
+// inst.on('itemsChanged', ({ detail: { overflowCount } }) => {
+//   console.log('We now have this many items in the overflow: ', overflowCount);
+// });
