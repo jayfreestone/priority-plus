de3684fb9cc3d665e6b56b0de1891f7780c28d55
Jay Freestone <jay.freestone@red-badger.com>
2019-06-06 10:39:44 +0100

    Set up Cypress TS transpilation.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,17 +1,22 @@
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
+const wp = require('@cypress/webpack-preprocessor');
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+module.exports = (on) => {
+  const options = {
+    webpackOptions: {
+      resolve: {
+        extensions: ['.ts', '.js'],
+      },
+      module: {
+        rules: [
+          {
+            test: /\.ts$/,
+            loader: 'ts-loader',
+            options: { transpileOnly: true },
+          },
+        ],
+      },
+    },
+  };
 
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+  on('file:preprocessor', wp(options))
+};
