64b837051dc029cf8b363eccc1c3d68da5d2cd00
Jay Freestone <jay.freestone@red-badger.com>
2019-06-06 10:16:21 +0100

    Add itemsChanged test.

--- cypress/integration/pplus.js
+++ cypress/integration/pplus.js
@@ -14,26 +14,35 @@ function registerContext() {
 
 describe('Events', () => {
   describe('fires', () => {
-    // it('itemsChanged', () => {
-    //   const itemsChangedCB = cy.spy();
+    it('itemsChanged', () => {
+      const itemsChangedCB = cy.spy();
 
-    //     // We need this get to ensure we have fully initialized
-    //   cy.get('@toggle-btn')
-    //     .should('not.be.visible')
-    //     .get('@instance')
-    //     .invoke('on', 'itemsChanged', itemsChangedCB)
-    //     .viewport(768, 660)
-    //     .get('@toggle-btn')
-    //     .should('be.visible')
-    //     .then(() => {
-    //       expect(itemsChangedCB).to.be.calledOnce;
-    //     });
-    // });
-
-    it('showOverflow', () => {
       cy.visit('/');
       registerContext();
+
+      // We need this get to ensure we have fully initialized
+      cy.get('@toggle-btn')
+        .should('not.be.visible')
+        .get('@instance')
+        .invoke('on', 'itemsChanged', itemsChangedCB)
+        // @todo: This is rubbish and should be re-thought.
+        // I believe we need the wait as otherwise the event is fired before
+        // the eventReady flag is set (i.e. before the first listener is
+        // established).
+        .wait(1)
+        .viewport(320, 660)
+        .get('@toggle-btn')
+        .should('be.visible')
+        .then(() => {
+          expect(itemsChangedCB).to.be.calledOnce;
+        });
+    });
+
+    it('showOverflow', () => {
       const showOverflowCB = cy.spy();
+
+      cy.visit('/');
+      registerContext();
 
       cy.get('@instance')
         .invoke('on', 'showOverflow', showOverflowCB)
@@ -47,13 +56,16 @@ describe('Events', () => {
     });
 
     it('hideOverflow', () => {
-      cy.visit('/?showOverflow=true');
-      registerContext();
       const hideOverflowCB = cy.spy();
 
-      cy.viewport(768, 660)
+      // Force overflow so we can see the dropdown
+      cy.viewport(768, 660);
+      // Show dropdown by default
+      cy.visit('/?showOverflow=true');
+      registerContext();
+
         // We need this get to ensure we have fully initialized
-        .get('@overflow-nav')
+      cy.get('@overflow-nav')
         .should('be.visible')
         .get('@instance')
         .invoke('on', 'hideOverflow', hideOverflowCB)
