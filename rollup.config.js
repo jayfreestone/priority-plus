14e4e2f6aadaccaa1c91434332c02a9e0c356407
Jay Freestone <jay.freestone@red-badger.com>
2019-06-12 13:56:23 +0100

    chore(rollup): Remove unused deps.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,3 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
