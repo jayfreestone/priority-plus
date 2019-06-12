cac9a243bd0bca2f6fe62af579d5d2c87dba8214
Jay Freestone <jay.freestone@red-badger.com>
2019-06-12 13:47:18 +0100

    refactor(rollup): Clean up config.

--- rollup.config.js
+++ rollup.config.js
@@ -5,41 +5,16 @@ import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 export default [
-  // browser-friendly UMD build
-  {
-    input: 'src/priorityPlus.ts',
-    output: {
-      name: 'priorityPlus',
-      file: pkg.browser,
-      format: 'umd'
-    },
-    plugins: [
-      typescript({
-        target: 'ES5',
-      }),
-      resolve(), // so Rollup can find `ms`
-      commonjs(), // so Rollup can convert `ms` to an ES module
-      terser(),
-    ]
-  },
-
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
   {
     input: 'src/priorityPlus.ts',
     output: [
       { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' }
+      { file: pkg.module, format: 'es' },
+      { name: 'priorityPlus', file: pkg.browser, format: 'umd' },
     ],
     plugins: [
-      typescript({
-        target: 'ES6',
-      }),
+      typescript(),
       terser(),
-    ]
-  }
+    ],
+  },
 ];
